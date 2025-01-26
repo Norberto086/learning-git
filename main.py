8b291a61c00dda36a7f328e05c1c23f10206bd43
Norbert Spychala <norbert086.ns@gmail.com>
2025-01-26 19:32:32 +0000

    Add main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+available_cheese = "e"
+if available_cheese == "edam":
+    print("Kupuję edam")
+elif available_cheese == "brie":
+    print ("Kupuje brie")
+elif available_cheese == "gouda":
+    print ("Kupuje gouda")
+else :
+    print ("dzwon do szefa")
