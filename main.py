743e6ade4f91e73c91a677341c26056796bfc5c4
Norbert Spychala <norbert086.ns@gmail.com>
2025-01-27 19:28:36 +0000

    finished

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ for sklep, product in shopping.items():
     shop_capitalize = sklep.capitalize()
     product_capitalize = [product.capitalize() for product in product]
 
-#     print(f"Ide do {shop_capitalize} i kupuje tam {product_capitalize}.")
-#     product_count = product_count + len(product_capitalize)
+    print(f"Ide do {shop_capitalize} i kupuje tam {product_capitalize}.")
+    product_count = product_count + len(product_capitalize)
 
-# print (f"W sumie kupuje {product_count} produktow")
+print (f"W sumie kupuje {product_count} produktow")
